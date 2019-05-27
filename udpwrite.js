adc1eb0dfed60185ad1e2b471a08260da13bd9c8
casktCase <caseyrudge@gmail.com>
2019-05-27 16:13:53 -0600

    Adding postgresql libraries and insert in updwrite.js

--- udpwrite.js
+++ udpwrite.js
@@ -1,26 +1,52 @@
 ﻿var dgram = require("dgram");
 var server = dgram.createSocket("udp4");
 var fs = require('fs');
+var pg = require('pg');
+//var db = require('db');
+const { Pool, Client } = require('pg')
+const connectionString = 'postgresql://postgres:password@127.0.0.1:5432/host'
+const pool = new Pool({ connectionString: connectionString, })
+var tempRead = '';
+var meterRead = '';
 
 var crlf = new Buffer(2);
 crlf[0] = 0xD; //CR - CR character
 crlf[1] = 0xA; //LF - LF character
 
 server.on("message", function (msg, rinfo) { //every time new data arrives do this:
-  console.log("server got: " + msg.readUInt16LE(0) + " from " + rinfo.address + ":" + rinfo.port); // you can comment this line out
-  fs.appendFile('mydata.txt', msg.readUInt16LE(0) + crlf, encoding = 'utf8', (err) => {
+  tempRead = msg.toString().substring(0, 5);
+  meterRead = msg.toString().substring(5, 10);
+  //console.log("server: " + msg.toString() + " from " + rinfo.address + ":" + rinfo.port); // you can comment this line out
+  console.log("temp: " + tempRead + " water: " + meterRead + " msg: " + msg.toString() + " from " + rinfo.address + ":" + rinfo.port); // you can comment this line out
+  fs.appendFile('mydata.txt', msg.toString() + crlf, encoding = 'utf8', (err) => {
   });//write the value to file and add CRLF for line break
-
   ///get value of sensors, log it to db and process for alerts
 
+  const query = {
+    text: 'INSERT INTO temps (reading, time) VALUES ($1, $2) RETURNING *;',
+    values: [tempRead, meterRead]
+  };
+
+  return new Promise(function (resolve, reject) {
+    pool.query(query, (err, res) => {
+      if (err) {
+        logger.error('Error saving to db: ' + err);
+        console.log('Error saving to db: ' + err);
+        reject(0)
+        //return 0;
+      }
+      else {
+        console.log('id' + res.rows[0].smsid) //This gives me the value to console.
+        resolve(res.rows[0].smsid)
+        // return res.rows[0].smsid;
+      }
+    })
+  })
 });
 
 server.on("listening", function () {
   var address = server.address();
   console.log("server listening " + address.address + ":" + address.port);
-
-
-
 });
 
 server.bind(6000); //listen to udp traffic on port 6000
